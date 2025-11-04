fa2ff55b71850c0f42e00f847b68645350d524c5
Berry de Vos <bdevos@gmail.com>
2025-11-04 10:23:06 +0100

    Resolve iOS build warnings by excluding certain headers with the modulemap

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.7.1
+// swift-tools-version:6.2
 import PackageDescription
 
 let package = Package(
     name: "ZXingCpp",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
         .library(
